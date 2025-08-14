e6f311150dce1b13ad1baeda2bd377a8d3f0c735
Pankaj Sarkar <pankaj sarkar>
2025-08-14 23:11:18 +0530

    AnimatedSearchInput

--- src/assets/icons/search-icon.tsx
+++ src/assets/icons/search-icon.tsx
@@ -1,36 +1,32 @@
-import React from 'react';
-import Svg, {Path} from 'react-native-svg';
-import {ViewStyle} from 'react-native';
+import * as React from "react";
 
-interface SearchIconProps {
+interface SearchIconProps extends React.SVGProps<SVGSVGElement> {
   width?: number;
   height?: number;
   color?: string;
-  style?: ViewStyle;
 }
 
-const SearchIcon: React.FC<SearchIconProps> = ({
+export const SearchIcon: React.FC<SearchIconProps> = ({
   width = 24,
   height = 24,
-  color = '#000',
-  style,
+  color = "#000",
+  ...props
 }) => {
   return (
-    <Svg
+    <svg
       width={width}
       height={height}
       viewBox="0 0 24 24"
       fill="none"
-      style={style}>
-      <Path
+      {...props}
+    >
+      <path
         d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
         stroke={color}
         strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </Svg>
+    </svg>
   );
 };
-
-export default SearchIcon;
